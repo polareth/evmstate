b9fc97e814f2e80b2725477e1b247d122479bf1d
0xpolarzero <0xpolarzero@gmail.com>
2025-04-11 12:35:57 +0200

    refactor: structure

--- test/contracts/index.ts
+++ test/contracts/index.ts
@@ -1,8 +1,8 @@
-export { AssemblyStorage } from "./advanced/AssemblyStorage.s.sol";
-export { StoragePacking } from "./basic/StoragePacking.s.sol";
-export { Arrays } from "./data-structures/Arrays.s.sol";
-export { Mappings } from "./data-structures/Mappings.s.sol";
-export { Structs } from "./data-structures/Structs.s.sol";
+export { AssemblyStorage } from "./base/AssemblyStorage.s.sol";
+export { StoragePacking } from "./base/StoragePacking.s.sol";
+export { Arrays } from "./base/Arrays.s.sol";
+export { Mappings } from "./base/Mappings.s.sol";
+export { Structs } from "./base/Structs.s.sol";
 export { DelegateBase } from "./delegate-calls/DelegateBase.s.sol";
 export { DelegateLogic } from "./delegate-calls/DelegateLogic.s.sol";
 export { ContractA } from "./interactions/ContractA.s.sol";
