77c1f86f3db50e44834320f922e8766522f8f5e6
0xpolarzero <0xpolarzero@gmail.com>
2025-05-08 23:37:21 +0200

    refactor: unsupported & experimental tags, refactor watchState client refork

--- src/lib/watch/index.ts
+++ src/lib/watch/index.ts
@@ -1,6 +1,6 @@
 import { type Address } from "tevm/utils";
 import { abi } from "@shazow/whatsabi";
-import { toFunctionSignature } from "viem";
+import { createPublicClient, http, toFunctionSignature } from "viem";
 
 import { labelStateDiff } from "@/lib/explore/label.js";
 import { type DeepReadonly } from "@/lib/explore/types.js";
@@ -15,15 +15,37 @@ import { type StateChange, type WatchStateOptions } from "@/lib/watch/types.js";
  *
  * Note: your RPC will need to support `debug_traceBlock`, which we use to get the pre/post state diff.
  *
+ * Note: currently, rebase mode is not supported in Tevm; if you create a fork client yourself and want to follow the
+ * latest state, you will need to refork the client on every block. Or you can just provide a rpc url and the common so
+ * it's handled for you.
+ *
  * @param options - {@link WatchStateOptions}
  * @returns Promise<() => void> - Unsubscribe function
+ * @experimental
  */
 export const watchState = async <TStorageLayout extends DeepReadonly<SolcStorageLayout> | undefined = undefined>(
   options: WatchStateOptions<TStorageLayout>,
 ): Promise<() => void> => {
   const { address, onStateChange, onError, pollingInterval = 1000 } = options;
   let { storageLayout, abi } = options;
-  const client = options.client ?? createClient(options);
+
+  // Create a proxy wrapper for the client if not provided
+  let internalClient = options.client ?? createClient(options);
+  const client =
+    options.client ??
+    new Proxy({} as typeof internalClient, {
+      get(_, prop) {
+        return Reflect.get(internalClient, prop);
+      },
+    });
+
+  // Currently, we need a public client to watch blocks
+  const watchClient = !options.client
+    ? createPublicClient({
+        chain: client.chain,
+        transport: http(client.chain?.rpcUrls.default.http[0]),
+      })
+    : undefined;
 
   if (!abi || !storageLayout) {
     const contractInfo = (
@@ -46,12 +68,18 @@ export const watchState = async <TStorageLayout extends DeepReadonly<SolcStorage
       return func;
     });
 
-  const unsubscribe = client.watchBlocks({
+  // If the user didn't provide a client, as long as rebase mode is not supported, we want to follow a public client
+  const unsubscribe = (options.client ? client : watchClient!).watchBlocks({
     onError,
     pollingInterval,
-
     includeTransactions: true,
     onBlock: async (block) => {
+      // Refork the client to get the latest state
+      // We're in fork mode (need to refork as blocks come in, as long as rebase mode is not supported in Tevm),
+      // if the user didn't provide a client, or if they provided a client in fork mode
+      if (!options.client) internalClient = createClient(options);
+
+      console.log(block.transactions);
       const traces = await debugTraceBlock(client, block.hash);
       traces.forEach(({ txHash, stateDiff, addresses, newAddresses, structLogs }) => {
         const uniqueAddresses = [...new Set([...addresses, ...newAddresses])].map((addr) => addr.toLowerCase());
